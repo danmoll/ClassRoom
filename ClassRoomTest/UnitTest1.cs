981ad6153056f14034fdc9d4c6437c1ca83fc897
DanMoll <dani948d@edu.easj.dk>
2019-02-15 12:35:30 +0100

    Opsatte UnitTest project.

---
+++ ClassRoomTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ClassRoomTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
